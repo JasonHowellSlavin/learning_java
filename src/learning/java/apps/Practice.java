1207a196478fb24589d9c61cc341612d2347867b
Jason Slavin <jslavin@marketo.com>
2022-05-04 15:30:36 -0700

    Basics review up to Arrays

---
+++ src/learning/java/apps/Practice.java
@@ -0,0 +1,33 @@
+package learning.java.apps;
+
+public class Practice {
+    public static void main(String[] args) {
+        String helloSentence = "Hello from the IDE inside learning.java,apps";
+        String[] breakfast = new String[]{"eggs", "bacon", "toast"};
+
+        printS(helloSentence + "Static method");
+
+        Practice myPractice = new Practice();
+        myPractice.print("Instance method");
+
+        for(int i = 0; i < breakfast.length; i++) {
+            printS(breakfast[i]);
+        }
+
+        breakfast[0] = "apple";
+
+        for(int i = 0; i < breakfast.length; i++) {
+            printS(breakfast[i]);
+        }
+
+    }
+
+    // Static methods are attached to a class, non static methods are attached to an instance of a class
+    static void printS(String data) {
+        System.out.println(data);
+    }
+
+    void print(String data) {
+        System.out.println(data);
+    }
+}
